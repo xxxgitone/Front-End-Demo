301aa9e6b57db676cfb1271f8bd9edcf50bbeb0b
xxxgitone <xxxgit@sina.com>
2016-11-05 17:26:21 +0800

    修改动画bug

--- 幽灵按钮/js/script.js
+++ 幽灵按钮/js/script.js
@@ -2,22 +2,20 @@ $(function() {
 	$('.link .button').hover(function() {
 		var title = $(this).attr('data-title');
 		$('.tip em').text(title);
-		var pos=$(this).position().left;
+		var pos = $(this).position().left;
 		var dis = ($('.tip').outerWidth() - $(this).outerWidth()) / 2;
 		var l = pos - dis;
 		$('.tip').css({
-			'left': l+ 'px',
-		}).animate({
+			'left': l + 'px',
+		}).stop(false,true).animate({
 			'top': 133,
 			'opacity': 1,
 		}, 300);
 	}, function() {
-		if(!$('.tip').is(':animated')) {
-			$('.tip').animate({
-				'top': 100,
-				'opacity': 0,
-			}, 300);
-		}
+		$('.tip').stop().animate({
+			'top': 100,
+			'opacity': 0,
+		}, 300);
 	})
 
 })
